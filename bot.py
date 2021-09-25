f6d014e9d8b9ad30031fced271636c5a6c5ca7d3
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2021-09-24 23:22:50 -0300

    Inicio

--- bot.py
+++ bot.py
@@ -9,3 +9,5 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 bot = telegram.Bot(token=telegram_token)
 # mande uma mensagem
 bot.sendMessage(chat_id=chat_id, text='O bot diz oi!')
+
+test
